4e2f47500018a3b9f5cc01845ab073ea65ef72bc
rishab Bansal <rishab780@gmail.com>
2017-04-24 14:42:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rishab/assignment/ModelClass/PlayersModelClass.java
@@ -0,0 +1,68 @@
+package com.example.rishab.assignment.ModelClass;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Rishab on 20-04-2017.
+ */
+
+public class PlayersModelClass implements Parcelable {
+    private String mName;
+    /**
+     *
+     * @param in sending in
+     */
+    protected PlayersModelClass(final Parcel in) {
+        mName = in.readString();
+    }
+
+    public final static Creator<PlayersModelClass> CREATOR = new Creator<PlayersModelClass>() {
+        @Override
+        public PlayersModelClass createFromParcel(final Parcel in) {
+            return new PlayersModelClass(in);
+        }
+
+        @Override
+        public PlayersModelClass[] newArray(final int size) {
+            return new PlayersModelClass[size];
+        }
+    };
+
+
+    /**
+     *
+     * @param mName name
+     */
+    public PlayersModelClass(final String mName) {
+        this.mName = mName;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(final Parcel dest, final int flags) {
+        dest.writeString(mName);
+
+    }
+
+    /**
+     *
+     * @return name
+     */
+    public String getmName() {
+        return mName;
+    }
+
+    /**
+     *
+     * @param mName name
+     */
+
+    public void setmName(final String mName) {
+        this.mName = mName;
+    }
+}
